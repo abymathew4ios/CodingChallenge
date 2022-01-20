ef64c63419e1dba6887a326a18ee759886653ecb
Aby Mathew <abymathew4ios@gmail.com>
2022-01-20 22:13:34 +0530

    Initial commit

---
+++ CodingChallenge/CodingChallenge/Pages/HomePage/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by WorkAccount on 20/01/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
