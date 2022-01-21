fc164b5bc2f684841632b81c2a385788a87d7f9c
Aby Mathew <abymathew4ios@gmail.com>
2022-01-21 14:38:22 +0530

    removing unused codes

--- CodingChallenge/CodingChallenge/Pages/HomePage/Models/Repository/HompeRepository.swift
+++ CodingChallenge/CodingChallenge/Pages/HomePage/Models/Repository/HompeRepository.swift
@@ -23,24 +23,6 @@ class HomeRepository {
         // Here we can collect data either from Local Storage or from Networkservice.
         
         // Fetching data from NetworkService
-//        guard let url = URL(string: "\(section)/\(period).json?", relativeTo: URL(string: BASE_URL)) else {return}
-//        let urlRequest = URLRequest(url: url)
-        
-//        var components = URLComponents()
-//            components.scheme = SCHEME_VALUE
-//            components.host = HOST
-//            components.path = PATH + "/\(section)/\(period).json"
-//            components.queryItems = [
-//                URLQueryItem(name: "api-key", value: API_KEY),
-//            ]
-//
-//            // Getting a URL from our components is as simple as
-//            // accessing the 'url' property.
-//        guard let url = components.url else { return completion(.failure(NetworkError.invalidUrl))}
-//        var urlRequest = URLRequest(url: url)
-//        urlRequest.httpMethod = "GET"
-
-        
         var request = URLRequest(url: URL(string: "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?&api-key=2XRnYxQeqdA8LnOdy6TBbTx8xYfGzWli")!,timeoutInterval: Double.infinity)
         request.httpMethod = "GET"
         
