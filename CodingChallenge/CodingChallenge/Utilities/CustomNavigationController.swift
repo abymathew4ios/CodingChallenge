c761713c9f5896d1373a4842c23189099e69a77f
Aby Mathew <abymathew4ios@gmail.com>
2022-01-21 14:10:42 +0530

    unwanted code delete

--- CodingChallenge/CodingChallenge/Utilities/CustomNavigationController.swift
+++ CodingChallenge/CodingChallenge/Utilities/CustomNavigationController.swift
@@ -9,19 +9,11 @@ import UIKit
 
 class CustomNavigationController: UINavigationController {
     
-  
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // set navigationBar title color
-        self.navigationController?.navigationBar.barTintColor = .green
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        
-        // Do any additional setup after loading the view.
     }
-    
-    
-    
     
 }
